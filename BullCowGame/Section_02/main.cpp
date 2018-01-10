69de819df875beb581d902262bf217d78b59d33d
hunter-morabito <hunter.morabito@gmail.com>
2018-01-09 23:39:32 -0500

    progress on BullCowGame

--- BullCowGame/Section_02/main.cpp
+++ BullCowGame/Section_02/main.cpp
@@ -1,4 +1,41 @@
+#include "main.h"
+
+// entry point
 int main() 
 {
+	PrintIntro();
+	PlayGame();
+	
+	cout << endl;
 	return 0;
+}
+
+void PrintIntro()
+{
+	// introduce the game
+	constexpr int WORD_LENGTH = 5;
+
+	cout << "Welcome to Bulls and Cows" << endl;
+	cout << "Try and guess the " << WORD_LENGTH;
+	cout << " letter isogram I'm thinking of." << endl;
+}
+
+void PlayGame()
+{
+	// loop for the number of turns asking for guesses
+	constexpr int NUMBER_OF_GUESSES = 5;
+	for (int i = 0; i < NUMBER_OF_GUESSES; i++)
+	{
+		string Guess = GetGuess();
+		cout << "Your guess was: " << Guess << endl;
+	}
+}
+
+string GetGuess() 
+{
+	string Guess;
+	cout << "Enter your guess: ";
+	// get a guess from the player
+	getline(cin, Guess);
+	return Guess;
 }
